bda67fc5c2bde47666447eeb0a88e7231c931f4a
Josep Ferrandis <josepfejo1@gmail.com>
2019-01-26 11:06:18 +0100

    Reorganizando carpetas para la entrega

--- Lab 1/dm_local.js
+++ Lab 1/dm_local.js
@@ -1,42 +1,57 @@
-var dm = require('./dm.js');
-                     
-exports.getSubjectList = function (cb) {
-	var res = dm.getSubjectList()
-    cb(res);
-}
+var dm = require ('./dm.js');
 
-// true on success
+// true if already exists
 exports.addUser = function (u,p, cb) {
-	var res = dm.addUser(u, p);
-	cb(res);
+	var exists = dm.addUser(u,p);
+	cb (exists);
 }
 
 // Adds a new subject to subject list. Returns -1 if already exists, id on success
 exports.addSubject = function (s, cb) {
-	var res = dm.addSubject(s);
-	cb(res);
+	var id = dm.addSubject (s);
+	cb (id);
+}
+
+exports.getSubjectList = function (cb) {
+	var list = dm.getSubjectList ();
+	cb (list);
 }
 
 exports.getUserList = function (cb) {
-	var res = dm.getUserList();
-	cb(res);
+	var list = dm.getUserList ();
+	cb (list);
 }
 
 // Tests if credentials are valid, returns true on success
 exports.login = function (u, p, cb) {
-    var res = dm.login(u, p);
-    cb(res);
+	var ok = dm.login (u,p);
+	cb (ok);
+}
+
+exports.addPrivateMessage = function (msg, cb){
+	dm.addPrivateMessage (msg);
+	cb();
+}
+
+exports.getPrivateMessageList = function (u1, u2, cb) {
+	var list = dm.getPrivateMessageList (u1,u2);
+	cb (list);
+}
+
+function getSubject (sbj, cb) {
+	var id = dm.getSubject (sbj);
+	cb (id);
 }
 
 // adds a public message to storage
 exports.addPublicMessage = function (msg, cb)
 {
-	dm.addPublicMessage(msg);
-	cb();
+	dm.addPublicMessage (msg);
+	cb ();
 }
 
 exports.getPublicMessageList = function (sbj, cb) {
-	var res = dm.getPublicMessageList(sbj);
-	cb(res);
+	var list = dm.getPublicMessageList (sbj);
+	cb (list);
 }
 
--- Lab 1/forum.js
+++ Lab 1/forum.js
@@ -2,15 +2,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var dm = require('./dm_local.js');
+var dm = require('./dm_remote.js');
 
 var viewsdir = __dirname + '/views';
 app.set('views', viewsdir)
 
+dm.Start(process.argv[3], process.argv[4]);
 
 // called on connection
 function get_page (req, res) {
- 	console.log ("Serving request " + req.params.page);
+	console.log ("Serving request " + req.params.page);
 	res.sendFile(viewsdir + '/' + req.params.page);
 }
 
@@ -27,94 +28,108 @@ app.get('/', function(req, res){
 	req.params.page = 'index.html'
 	get_page (req, res);
 });
+
 app.get('/:page', function(req, res){
 	get_page (req, res);
 });
 
-
-
 io.on('connection', function(sock) {
 	console.log("Event: client connected");
 	sock.on('disconnect', function(){
-    	console.log('Event: client disconnected');
-  	});
+		console.log('Event: client disconnected');
+	});
 
   	// on messages that come from client, store them, and send them to every 
   	// connected client
   	// TODO: We better optimize message delivery using rooms.
-	sock.on('message', function(msgStr){
-		console.log("Event: message: " + msgStr);
-		var msg = JSON.parse (msgStr);
+  	sock.on('message', function(msgStr){
+  		var msg = JSON.parse (msgStr);
 		msg.ts = new Date(); // timestamp
 		if (msg.isPrivate) {
-			//dm.addPrivateMessage (msg, function(){
-			//	io.emit('message', JSON.stringify(msg));
-			//});
+			dm.addPrivateMessage (msg, function () {
+				//io.emit('message', JSON.stringify(msg));
+				sock.emit('message', JSON.stringify(msg));
+			});
 		} else {
-			dm.addPublicMessage (msg, function(){
-				io.emit('message', JSON.stringify(msg));
+			dm.addPublicMessage (msg, function () {
+				//io.emit('message', JSON.stringify(msg));
+				sock.emit('message', JSON.stringify(msg));
 			});
 		}
-  	});
+	});
 
-  	// New subject added to storage, and broadcasted
-  	sock.on('new subject', function (sbj) {
-		dm.addSubject(sbj, function(x){
-			console.log("Event: new subject: " + sbj + '-->' + x);
-			if (x == -1) {
-				sock.emit ('new subject', 'err', 'El tema ya existe', sbj);
-			} else {
-				sock.emit ('new subject', 'ack', x, sbj);
-			   io.emit ('new subject', 'add', x, sbj); 			
-			}
-		});
-  	});
+    // New subject added to storage, and broadcasted
+    sock.on('new subject', function (sbj) {
+    	dm.addSubject(sbj, function (id) {
+    		console.log("Event: new subject: " + sbj + '-->' + id);
+    		if (id == -1) {
+    			sock.emit ('new subject', 'err', 'El tema ya existe', sbj);
+    		} else {
+				sock.emit ('new subject', 'ack', id, sbj);
+				// io.emit ('new subject', 'add', id, sbj);
+				sock.emit ('new subject', 'add', id, sbj);
+    		}      
+    	});
+    });
+
+    // New subject added to storage, and broadcasted
+    sock.on('new user', function (usr, pas) {
+    	dm.addUser(usr, pas, function (exists) {
+    		console.log("Event: new user: " + usr + '(' + pas + ')');
+    		if (exists) {
+    			sock.emit ('new user', 'err', usr, 'El usuario ya existe');
+    		} else {
+    			sock.emit ('new user', 'ack', usr);
+    			io.emit ('new user', 'add', usr);      
+    		}
+    	});
+    });
 
   	// Client ask for current user list
   	sock.on ('get user list', function () {
-		dm.getUserList(function (x){
-			console.log("Event: get user list");
-			sock.emit('user list', x);
-		});	
+  		dm.getUserList (function (list) {
+	  		console.log("Event: get user list");  		
+  			sock.emit ('user list', list);
+  		});
   	});
 
-  	// Client ask for current subject list
-  	sock.on ('get subject list', function () {
-		dm.getSubjectList(function (x){
-			console.log("Event: get subject list");  		
-			sock.emit ('subject list', x);
-		})
-  	});
+   	// Client ask for current subject list
+   	sock.on ('get subject list', function () {
+   		dm.getSubjectList (function (list) {
+	   		console.log("Event: get subject list");  		
+   			sock.emit ('subject list', list);
+   		});
+   	});
 
-  	// Client ask for message list
-  	sock.on ('get message list', function (from, to, isPriv) {
-		console.log("Event: get message list: " + from + ':' + to + '(' + isPriv + ')');  		
-		if (isPriv) {
-			// dm.getPrivateMessageList(function(x){
-			// 	sock.emit ('message list', from, to, isPriv, x);
-			// })
-	  	} else {
-			dm.getPublicMessageList(to, function(x){
-				sock.emit ('message list', from, to, isPriv, x);
-			});
-	  	}
+   	// Client ask for message list
+   	sock.on ('get message list', function (from, to, isPriv) {
+  		console.log("Event: get message list: " + from + ':' + to + '(' + isPriv + ')');  		
+  		if (isPriv) {
+  			dm.getPrivateMessageList (from, to, function (list) {
+  				sock.emit ('message list', from, to, isPriv, list);
+  			});
+  		} else {
+  			dm.getPublicMessageList (to, function (list) {
+	  			sock.emit ('message list', from, to, isPriv, list);
+	  		});
+  		}
   	});
 
   	// Client authenticates
   	// TODO: session management and possible single sign on
   	sock.on ('login', function (u,p) {
-		dm.login(u,p,function(x){
-			console.log("Event: user logs in");
-			if(!x){
-			 	console.log ("Wrong user credentials: " + u + '(' + p + ')');
-			 	sock.emit ('login', 'err', 'Credenciales incorrectas');
-			}else {
-			 	console.log ("User logs in: " + u + '(' + p + ')');
-			   	sock.emit ('login', 'ack', u);	  			
-			}
-		});	
+  		console.log("Event: user logs in");  		
+  		dm.login (u, p, function (ok) {
+  			if (!ok) {
+  				console.log ("Wrong user credentials: " + u + '(' + p + ')');
+  				sock.emit ('login', 'err', 'Credenciales incorrectas');
+  			} else {
+  				console.log ("User logs in: " + u + '(' + p + ')');
+  				sock.emit ('login', 'ack', u);	  			
+  			}
+  		});
   	});
 });
 
 // Listen for connections !!
-http.listen (10000, on_startup);
+http.listen (process.argv[2], on_startup);
